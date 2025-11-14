394ccad149bda346a8a00706e81facbe374c480f
San-43 <virgosmp.mx@gmail.com>
2025-11-14 12:48:37 -0600

    hangar

--- src/main/java/com/example/hangar/repository/HangarRepository.java
+++ src/main/java/com/example/hangar/repository/HangarRepository.java
@@ -3,11 +3,13 @@ package com.example.hangar.repository;
 import com.example.hangar.model.Hangar;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface HangarRepository extends JpaRepository<Hangar, Long> {
 
     @EntityGraph(attributePaths = "empresa")
+    @Query("SELECT h FROM Hangar h")
     List<Hangar> findAllWithEmpresa();
 }
