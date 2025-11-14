bc1635b66faa710a48ef4cc207ddeddec997286c
San-43 <virgosmp.mx@gmail.com>
2025-11-14 14:02:49 -0600

    hangar

--- src/main/java/com/example/hangar/model/Hangar.java
+++ src/main/java/com/example/hangar/model/Hangar.java
@@ -1,15 +1,7 @@
 package com.example.hangar.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -33,7 +25,7 @@ public class Hangar {
     @ManyToOne(fetch = FetchType.LAZY)
     private Empresa empresa;
 
-    @OneToOne(mappedBy = "hangar", fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "hangar", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private Encargado encargado;
 
     @OneToMany(mappedBy = "hangar")
